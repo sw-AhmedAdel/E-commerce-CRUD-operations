ce4ae9bf09fd6d5755d885e7fca9bb8253920ddc
ahmed adel <ahmed.adel@pelcro.com>
2022-12-28 17:55:21 +0200

    Delete unnecessary comments

--- server/src/redis.js
+++ server/src/redis.js
@@ -1,36 +1,3 @@
-/*
-
-async function httpGetAllProducts(req, res, next) {
-  client.get("products", async (error, products) => {
-    if (error) console.error(error);
-    if (products != null) {
-      return res.status(200).json({
-        status: "success",
-        results: JSON.parse(products).length,
-        products: JSON.parse(products),
-      });
-    } else {
-      console.log("feathing data");
-      const feature = new filter(req.query);
-      const { limit, skip } = feature.pagination();
-
-      const products = await GetAllProducts(
-        feature.filter(),
-        feature.sort("-createdAt"),
-        feature.fields(),
-        skip,
-        limit
-      );
-      client.SETEX("products", 3600, JSON.stringify(products));
-      return res.status(200).json({
-        status: "success",
-        results: products.length,
-        data: products,
-      });
-    }
-  });
-}
-*/
 const redis = require("redis");
 const client = redis.createClient({
   legacyMode: true,
